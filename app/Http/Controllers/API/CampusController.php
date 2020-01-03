ebb5d2c96fe09e42e814d2ec0e09878c74997206
hasan.siwi <hasan.siwi@gmail.com>
2020-01-03 23:54:05 +0300

    moved conreollers files to new directory

--- app/Http/Controllers/API/CampusController.php
+++ app/Http/Controllers/API/CampusController.php
@@ -1,62 +1,98 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
+use App\Campus;
+use App\Http\Helpers\CustomResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class CampusController extends Controller
 {
+    private $result = [];
+
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function index()
     {
-        //
-    }
+        $Campuses = Campus::all();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return CustomResponse::customResponse(
+            $Campuses,
+            CustomResponse::$successCode,
+            'successfully got the result'
+        );
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), $this->rules());
+
+        if ($validator->fails())
+        {
+            return CustomResponse::customResponse(
+                $validator->messages(),
+                CustomResponse::$unprocessableEntity
+            );
+        }
+
+        $validated = $validator->validated();
+
+        DB::beginTransaction();
+        try
+        {
+            Campus::create($validated);
+
+            DB::commit();
+
+            return CustomResponse::customResponse(
+                $validated,
+                CustomResponse::$successCode,
+                'the campus has been created successfully'
+            );
+        }
+        catch (\Exception $e)
+        {
+            DB::rollBack();
+            return CustomResponse::customResponse(
+                null,
+                CustomResponse::$successCode,
+                'the campus has NOT been created successfully'
+            );
+        }
     }
 
     /**
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function show($id)
     {
-        //
-    }
+        $campuses = Campus::find($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+        if ($campuses)
+        {
+            return CustomResponse::customResponse(
+                $campuses,
+                CustomResponse::$successCode,
+                'successfully got the campus'
+            );
+        }
+
+        return CustomResponse::customResponse(null, CustomResponse::$notFound,'nothing found' );
     }
 
     /**
@@ -75,10 +111,51 @@ class CampusController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy($id)
     {
-        //
+        $campus = Campus::find($id);
+
+        if ($campus == null)
+        {
+            return CustomResponse::customResponse(
+                null,
+                CustomResponse::$notFound,
+                'the campus is not found!'
+            );
+        }
+        DB::beginTransaction();
+        try
+        {
+            Campus::where('id', $id)->delete();
+            $this->result = $campus->delete();
+            DB::commit();
+            return CustomResponse::customResponse(
+                null,
+                CustomResponse::$successCode,
+                "the campus has been deleted successfully"
+            );
+        }
+        catch (\Exception $e)
+        {
+            DB::rollBack();
+            return CustomResponse::customResponse(
+                null,
+                CustomResponse::$errorCode,
+                "the campus has NOT been deleted successfully"
+            );
+        }
+    }
+
+    public function rules()
+    {
+        return [
+            'name'      => 'required|max:255',
+            'school_id' => 'required',
+            'email'     => 'email',
+            'phone'     => 'max:10',
+            'address'   => 'max:255',
+        ];
     }
 }
